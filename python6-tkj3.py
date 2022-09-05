dab3a31190daad9d5992a6cce5eeb47dce75c4e6
Zackpass <112367711+Zackpass@users.noreply.github.com>
2022-09-04 22:16:15 -0700

    Tugas pemdas
    Dzaki fasya XTKJ3

---
+++ python6-tkj3.py
@@ -0,0 +1,17 @@
+# dolar ke rupiah
+dolar = int(input("masukan nilai dolar: "))
+rupiah = dolar * 15000
+
+print  ("dolar ke rupiah" , rupiah)
+
+# euro ke won
+euro = int(input("masukan nilai euro: "))
+won = euro * 135610
+
+print  ("euro ke won" , won)
+
+# yen ke rupiah
+yen = int(input("masukan nilai yen: "))
+rupiah = yen * 106
+
+print  ("yen ke rupiah" , yen)
