9827cc215e0fd9eac7e9f41b28cad043429c5b10
Zackpass <112367711+Zackpass@users.noreply.github.com>
2022-09-07 23:45:13 -0700

    python latihan
    Dzaki Fasya XTKJ3

---
+++ Python - latihan.py
@@ -0,0 +1,28 @@
+#persegi
+sisi = int(input("masukan nilai sisi:"))
+luas_p = sisi * sisi
+print("luas persegi adalah:", luas_p)
+
+#persegi panjang
+panjang = int(input("masukan nilai panjang:"))
+lebar = int(input("masukan nilai lebar"))
+luas_pj = panjang * lebar
+print("luas persegi panjang adalah:", luas_pj)
+
+#jajar genjang
+alas = int(input("masukan nilai alas :"))
+tinggi = int(input("masukan nilai tinggi :"))
+luas_jg = alas* tinggi
+print("luas jajar genjang adalah:", luas_jg)
+
+#lingkaran
+r= int(input("masukan nilai r:"))
+phi =22/7
+luas_l= phi * r * r
+print("luas lingkaran adalah:",luas_l)
+
+#segitiga
+alas = int(input("masukan nilai alas:"))
+tinggi = int(input("masukan nilai tinggi:"))
+luas_s = 1/2 * alas * tinggi
+print("luas segitiga adalah:", luas_s)
