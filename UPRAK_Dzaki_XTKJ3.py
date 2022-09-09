b48b72b40815400a9bada9e7ad461ba28785858c
Zackpass <112367711+Zackpass@users.noreply.github.com>
2022-09-08 19:49:06 -0700

    UPRAK PRAKTIKUM
    Dzaki fasya X TKJ 3

---
+++ UPRAK_Dzaki_XTKJ3.py
@@ -0,0 +1,34 @@
+#buatlah sintaks menghitung volume bangun ruang
+
+#balok
+panjang = int(input("panjang :"))
+lebar = int(input("lebar :"))
+tinggi = int(input("tinggi :"))
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah :", volume_b)
+
+#kubus
+sisi = int(input("sisi :"))
+sisi = int(input("sisi :"))
+sisi = int(input("sisi :"))
+volume_k = sisi * sisi * sisi
+print("volume kubus adalah :", volume_k)
+
+#limas
+1/3
+luas_alas_persegi = int(input("luas alas persegi :"))
+tinggi = int(input("tinggi :"))
+volume_l = 1/2 * luas_alas_persegi * tinggi
+print("volume limas adalah :", volume_l)
+
+#tabung
+22/7
+r = int(input("r :"))
+r = int(input("r :"))
+t = int(input("t :"))
+
+#celcius to reamur
+celcius = int(input("masukan suhu celcius:"))
+reamur = (4/5 * celcius)
+print(celcius, "celcius adalah", reamur, "reamur")
+            
